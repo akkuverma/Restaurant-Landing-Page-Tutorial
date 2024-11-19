e16ccb1df7693ee467e862ed1b3fea3acb4c5f66
akkuverma <46926845+akkuverma@users.noreply.github.com>
2024-11-19 19:07:50 +0530

    Update App.js
    delete the 6 nd 7 line

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Home from "./Components/Home";
 import About from "./Components/About";
 import Work from "./Components/Work";
 import Testimonial from "./Components/Testimonial";
-import Contact from "./Components/Contact";
-import Footer from "./Components/Footer";
+
 
 function App() {
   return (
